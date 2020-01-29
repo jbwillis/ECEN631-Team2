3dd0fbfd30d65660c7abd58011391180491475dc
Jacob Willis <jbwillis272@gmail.com>
2020-01-28 22:11:40 -0700

    some segmentation things I tried

--- VisualInspection/preprocess.py
+++ VisualInspection/preprocess.py
@@ -24,10 +24,30 @@ rawVideo = cv2.VideoCapture(args.input_file)
 
 backSub = cv2.createBackgroundSubtractorMOG2()
 
+def segment(img):
+    """
+    """
+    blurred = cv2.GaussianBlur(img, (15, 15), 0)
+    fgMask = backSub.apply(blurred)
+
+    processed = fgMask
+    # convert to grayscale
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # gray = cv2.adaptiveThreshold(gray, 127, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 1)
+    # processed = gray
+
+    # processed, contours, hierarchy = cv2.findContours(frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    return processed
+
+
 while (rawVideo.isOpened()):
     ret, frame = rawVideo.read()
 
+    processed = segment(frame)
+
     cv2.imshow('Raw Video', frame)
+    cv2.imshow('Processed', processed)
     cv2.waitKey(15)
 
 rawVideo.release()
