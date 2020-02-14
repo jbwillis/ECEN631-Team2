9227dbbf281903339e455f37c94922a3f06e46cf
Jake Johnson <jacobcjohnson13@gmail.com>
2020-02-14 11:26:22 -0700

    now shows points in xy space

--- autonomousCar/obstacle_map.py
+++ autonomousCar/obstacle_map.py
@@ -15,6 +15,7 @@ def main():
     index = 0
     ret, frame = vid.read()
     depth = depth_list[index]
+    image_width = depth.shape[1]
 
     while ret:
 
@@ -22,6 +23,21 @@ def main():
 
         cone_depth = cv.bitwise_and(depth, depth, mask=cones)
         wall_depth = cv.bitwise_and(depth, depth, mask=walls)
+
+        num_samples = 300
+        # wall_pix = cv.findNonZero(walls)
+        wall_pix = np.argwhere(walls != 0)
+        if wall_pix.size != 0:
+            np.random.shuffle(wall_pix)
+            wall_pts = wall_pix[:num_samples,:]
+            wall_pts_r = depth[wall_pts[:,0],wall_pts[:,1]]
+            wall_pts_b = (wall_pts[:,1] - image_width/2.0)/image_width * np.pi/4
+            wall_pts_x = wall_pts_r * np.sin(wall_pts_b)
+            wall_pts_y = wall_pts_r * np.cos(wall_pts_b)
+            plt.scatter(wall_pts_x, wall_pts_y)
+            plt.show()
+
+            # wall_pts_depth = depth[]
 
         cv.imshow('cones', cones)
         cv.imshow('walls', walls)
